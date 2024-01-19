84f8a4ee43124ecb83060f8d8f39163990143931
Hunter Nelson <huntnel3@byu.edu>
2024-01-18 17:02:50 -0700

    Initial frontend addition of the code editor

--- frontend/src/components/CodeEditor.js
+++ frontend/src/components/CodeEditor.js
@@ -1,5 +1,22 @@
+import CodeMirror from '@uiw/react-codemirror';
+import React from 'react';
+import { andromeda } from '@uiw/codemirror-theme-andromeda';
+import { langs } from '@uiw/codemirror-extensions-langs';
+
 function CodeEditor() {
-  return <div>Code Editor</div>;
+  const [value, setValue] = React.useState("print('hello world!');");
+  const onChange = React.useCallback((val) => {
+    setValue(val);
+  }, []);
+
+  const handleButtonClick = (code) => {
+    console.log("code: " + code);
+  };
+
+  return <div>
+    <CodeMirror onSubmit={handleButtonClick} theme={andromeda} value={value} height="200px" extensions={[langs.python()]} onChange={onChange} />
+    <button onClick={() => handleButtonClick(value)}>Run</button>
+  </div>;
 }
 
 export default CodeEditor;

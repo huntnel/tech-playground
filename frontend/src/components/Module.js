d278c7d23939bb0ee380c00d1577ba496fd7765d
Hunter Nelson <huntnel3@byu.edu>
2024-02-15 09:39:52 -0700

    improved the load time of the Module component

--- frontend/src/components/Module.js
+++ frontend/src/components/Module.js
@@ -1,28 +1,20 @@
 import Task from './Task';
-import React from 'react';
+import {useMemo, useState, memo} from 'react';
 import getModuleData from '../moduleData';
-function Module(props) {
-    const data = getModuleData(props.moduleNumber);
-    const [currentTask, setCurrentTask] = React.useState(1);
+
+const Module = memo((props) => {
+    const { moduleNumber } = props;
+    const data = useMemo(() => getModuleData(moduleNumber), [moduleNumber]);
+    const [currentTask, setCurrentTask] = useState(1);
 
     const handleTaskChange = (level) => {
         setCurrentTask(level);
     }
 
-    const taskComponents = [];
-    for (let iCount = 0; iCount < data.totalTasks; iCount++) {
-        taskComponents.push(<Task changeTask={handleTaskChange} image={data.images[iCount]} moduleNumber={props.moduleNumber} totalTasks={data.totalTasks} currentTask={iCount + 1} initialComment={data.comments[iCount]} message={data.messages[iCount]} />);
-    }
-
-    const renderCurrentTask = () => {
-        return taskComponents[currentTask - 1];
-    };
-
     return (
         <div>
-            {renderCurrentTask()}
+            <Task key={currentTask} changeTask={handleTaskChange} image={data.images[currentTask - 1]} moduleNumber={moduleNumber} totalTasks={data.totalTasks} currentTask={currentTask} initialComment={data.comments[currentTask - 1]} message={data.messages[currentTask - 1]} />
         </div>
     );
-}
-
+});
 export default Module;

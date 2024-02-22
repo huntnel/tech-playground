eb7faab712e962a858b11fd68b001383de209b52
Hunter Nelson <huntnel3@byu.edu>
2024-02-22 09:22:19 -0700

    improved grid layout

--- frontend/src/components/ModuleHome.js
+++ frontend/src/components/ModuleHome.js
@@ -13,83 +13,98 @@ function ModuleHome() {
     return (
         <div>
             <Container style={{ height: '100vh', overflow: 'auto', backgroundColor: "#000000" }} fluid>
-                <Row>
-                    <Col xs={0} sm={0} md={1} className='text-white'>
-                        Hi
+                <Row className='pb-sm-5'>
+                    <Col>
+                        <h1 className='logo-color-dark text-center pixel-font'>Python Quests</h1>
                     </Col>
-                    <Col className="d-flex flex-column">
-                        <Row><h1 className='logo-color-dark text-center pixel-font'>Python Quests</h1></Row>
-                        <Row className='my-5'></Row>
-                        <Row>
-                            <Container className='d-flex card-module-container'>
-                                <Container onClick={() => navigateToModule("/module1")}>
-                                    <Card border="light" className='module-card border-0'>
-                                        <Card.Body>
-                                            <Card.Title className='text-white pixel-font card-title text-center'>Module 1</Card.Title>
-                                        </Card.Body>
-                                        <Card.Img className='card-img' variant="bottom" src="img/module1/Wizard.jpg" />
-                                        <div className='card-img-overlay'>
-                                            <div className="overlay-text">
-                                                <Card.Text className='position-absolute text-center pixel-font'>
-                                                    <br></br>Learn about print statements and data types!
-                                                </Card.Text>
-                                            </div>
-                                        </div>
-                                    </Card>
-                                </Container>
-
-                                <Container onClick={() => navigateToModule("/module2")}>
-                                    <Card border="light" className='module-card border-0'>
-                                        <Card.Body>
-                                            <Card.Title className='text-white pixel-font card-title text-center'>Module 2</Card.Title>
-                                        </Card.Body>
-                                        <Card.Img className='card-img' variant="bottom" src="img/module4/Werewolf.png" />
-                                        <div className='card-img-overlay'>
-                                            <div className="overlay-text">
-                                                <Card.Text className='position-absolute text-center pixel-font'>
-                                                    <br></br>Learn about if statements!
-                                                </Card.Text>
-                                            </div>
-                                        </div>
-                                    </Card>
-                                </Container>
-
-                                <Container onClick={() => navigateToModule("/module3")}>
-                                    <Card border="light" className='module-card border-0'>
-                                        <Card.Body>
-                                            <Card.Title className='text-white pixel-font card-title text-center'>Module 3</Card.Title>
-                                        </Card.Body>
-                                        <Card.Img className='card-img' variant="bottom" src="img/module3/Witch.png" />
-                                        <div className='card-img-overlay'>
-                                            <div className="overlay-text">
-                                                <Card.Text className='inner-overlay-text pixel-font'>
-                                                    <br></br>Learn about loops!
-                                                </Card.Text>
-                                            </div>
-                                        </div>
-                                    </Card>
-                                </Container>
-
-                                <Container onClick={() => navigateToModule("/module4")}>
-                                    <Card border="light" className='module-card border-0'>
-                                        <Card.Body>
-                                            <Card.Title className='text-white pixel-font card-title text-center'>Module 4</Card.Title>
-                                        </Card.Body>
-                                        <Card.Img className='card-img' variant="bottom" src="img/module4/Vampire.png" />
-                                        <div className='card-img-overlay'>
-                                            <div className="overlay-text">
-                                                <Card.Text className='position-absolute text-center pixel-font'>
-                                                    <br></br>Advanced Topics!
-                                                </Card.Text>
-                                            </div>
-                                        </div>
-                                    </Card>
-                                </Container>
-                            </Container>
-                        </Row>
-                    </Col>
-                    <Col xs={0} sm={0} md={1} className='text-white'>Hi</Col>
                 </Row>
+                <Row className='pt-sm-5'>
+                    <Col className="d-flex flex-column">
+                        <Container className='d-flex card-module-container'>
+                            <Container onClick={() => navigateToModule("/module1")}>
+                                <Card border="light" className='module-card border-0'>
+                                    <Card.Body>
+                                        <Card.Title className='text-white pixel-font card-title text-center'>Module 1</Card.Title>
+                                    </Card.Body>
+                                    <Card.Img className='card-img' variant="bottom" src="img/module1/Wizard.jpg" />
+                                    <div className='card-img-overlay'>
+                                        <div className="overlay-text">
+                                            <Card.Text className='position-absolute text-center pixel-font'>
+                                                <br></br>Learn about print statements and data types!
+                                            </Card.Text>
+                                        </div>
+                                    </div>
+                                </Card>
+                            </Container>
+
+                            <Container onClick={() => navigateToModule("/module2")}>
+                                <Card border="light" className='module-card border-0'>
+                                    <Card.Body>
+                                        <Card.Title className='text-white pixel-font card-title text-center'>Module 2</Card.Title>
+                                    </Card.Body>
+                                    <Card.Img className='card-img' variant="bottom" src="img/module4/Werewolf.png" />
+                                    <div className='card-img-overlay'>
+                                        <div className="overlay-text">
+                                            <Card.Text className='position-absolute text-center pixel-font'>
+                                                <br></br>Learn about if statements!
+                                            </Card.Text>
+                                        </div>
+                                    </div>
+                                </Card>
+                            </Container>
+
+                            <Container onClick={() => navigateToModule("/module3")}>
+                                <Card border="light" className='module-card border-0'>
+                                    <Card.Body>
+                                        <Card.Title className='text-white pixel-font card-title text-center'>Module 3</Card.Title>
+                                    </Card.Body>
+                                    <Card.Img className='card-img' variant="bottom" src="img/module3/Witch.png" />
+                                    <div className='card-img-overlay'>
+                                        <div className="overlay-text">
+                                            <Card.Text className='inner-overlay-text pixel-font'>
+                                                <br></br>Learn about loops!
+                                            </Card.Text>
+                                        </div>
+                                    </div>
+                                </Card>
+                            </Container>
+
+                            <Container onClick={() => navigateToModule("/module4")}>
+                                <Card border="light" className='module-card border-0'>
+                                    <Card.Body>
+                                        <Card.Title className='text-white pixel-font card-title text-center'>Module 4</Card.Title>
+                                    </Card.Body>
+                                    <Card.Img className='card-img' variant="bottom" src="img/module4/Vampire.png" />
+                                    <div className='card-img-overlay'>
+                                        <div className="overlay-text">
+                                            <Card.Text className='position-absolute text-center pixel-font'>
+                                                <br></br>Advanced Topics!
+                                            </Card.Text>
+                                        </div>
+                                    </div>
+                                </Card>
+                            </Container>
+                        </Container>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={0} sm={0} md={1}>
+                        <LazyLoadImage 
+                            src='img/blackBackground.png'
+                            className='img-fluid'
+                            alt="Challenge"
+                            effect="blur"/>
+                    </Col>
+                    <Col></Col>
+                    <Col xs={0} sm={0} md={1}>
+                        <LazyLoadImage 
+                            src='img/blackBackground.png'
+                            className='img-fluid'
+                            alt="Challenge"
+                            effect="blur"/>
+                    </Col>
+                </Row>
+                
             </Container>
         </div>
     );
